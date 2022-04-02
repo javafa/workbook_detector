167d21f8f0111b93e7e4faa87f06b6f81d3fa9ce
javafa <nigne2016@gmail.com>
2022-04-02 22:14:02 +0900

    labeling

--- slice.py
+++ slice.py
@@ -2,14 +2,14 @@ import fitz
 import os
 
 # 해설 pdf
-answer_dir = ".pdf/answer"
+answer_dir = "./pdf/answer"
 
 # 문제 pdf
 content_dir = "./pdf/content"
 content_pdf_list = os.listdir(content_dir)
 
 # 
-matrix = fitz.Matrix(2.0, 2.0)
+matrix = fitz.Matrix(1.0, 1.0)
 
 file_idx = 0
 
@@ -17,10 +17,11 @@ for file in content_pdf_list :
     if file.endswith(".pdf") :
         file_idx += 1
         file_name = f'{file_idx}'.zfill(3)
+        
         doc = fitz.open(f'{content_dir}/{file}')
         for page in doc:
             pix = page.get_pixmap(matrix=matrix)
             page_number = f'{page.number}'.zfill(3)
             image_name = f'{file_name}_{page_number}.png'
-            pix.save(f'{content_dir}/sliced/{image_name}')
+            pix.save(f'{content_dir}/images/{image_name}')
             print("sliced ", image_name)
